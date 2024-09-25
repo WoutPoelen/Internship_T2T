7bbd26f7bf6cb791cf4e805be7589a5e10ea0cbe
WoutPoelen <Wout.Poelen@radboudumc.nl>
2024-09-25 09:34:04 +0200

    Added comments and docstrings to both mismatch and histogram plot files and updated the bash file for the indel balance.

--- coverage_occurrence_histogram/make_coverage_mean_histogram.py
+++ coverage_occurrence_histogram/make_coverage_mean_histogram.py
@@ -4,12 +4,21 @@ import pandas as pd
 from collections import Counter
 import matplotlib.pyplot as plt
 
-# T2T: C:\Users\Z659216\Desktop\BAM_files\test_run2_T2T.regions.bed.gz
-# Hg38: C:\Users\Z659216\Desktop\BAM_files\test_run_Hg38.regions.bed.gz
-
 def open_file():
+    """
+    This functions opens the two given bed files that have the coverage of the regions. This file should be made with
+    the bash file:
 
+    Args:
+    nothing
 
+    Returns:
+         t2t_dataframe(dataframe): a dataframe that contains the chromosome, start and end of the region.
+         Tt also has the coverage of the regions from the t2t bed file.
+         hg38_dataframe(dataframe: a dataframe that contains the chromosome, start and end of the region.
+         Tt also has the coverage of the regions from the hg38 bed file.
+    """
+    # Asks for the input path for the t2t bed regions file
     input_path_t2t = input("Give the path to the t2t bed regions file: ")
     print("given path: " + input_path_t2t)
 
@@ -17,14 +26,14 @@ def open_file():
     if os.path.exists(input_path_t2t):
         print("File exists")
         print(" ")
-    #     Opens the file and turns it into a pandas dataframe.
+        # Opens the file with gunzip and turns it into a pandas dataframe
         with gzip.open(input_path_t2t, "r") as f:
             t2t_dataframe = pd.read_csv(input_path_t2t, sep="\t")
 
     else:
         print("Nothing found in the file path")
 
-
+    # Asks for the input path for the t2t BAM file
     input_path_hg38 = input("Give the path to the GRCh38 bed regions file: ")
     print("given path: " + input_path_hg38)
 
@@ -32,6 +41,7 @@ def open_file():
     if os.path.exists(input_path_hg38):
         print("File exists")
         print(" ")
+        # Opens the file with gunzip and turns it into a dataframe
         with gzip.open(input_path_hg38, "r") as f:
             hg38_dataframe = pd.read_csv(input_path_hg38, sep="\t")
 
@@ -39,54 +49,77 @@ def open_file():
     else:
         print("Nothing found in the file path")
 
+    # Sets the column names for the dataframe.
     hg38_dataframe.columns = ["Chromosome", "Start", "End", "mean_coverage"]
     t2t_dataframe.columns = ["Chromosome", "Start", "End", "mean_coverage"]
 
 
-
     return t2t_dataframe, hg38_dataframe
 
-def making_bins(dataframe_t2t, dataframe_hg38):
+def Counting_the_coverage(dataframe_t2t, dataframe_hg38):
+    """
+    This functions turns the dataframe column "mean coverage" into a list and makes a dictionary with the amount of
+    coverage as the keys and the amount of times that coverage was performed in the file.
 
+    Args:
+        dataframe_t2t(dataframe): a dataframe that contains the chromosome, start and end of the region.
+        Tt also has the coverage of the regions from the t2t bed file.
+        dataframe_hg38(dataframe): a dataframe that contains the chromosome, start and end of the region.
+        Tt also has the coverage of the regions from the hg38 bed file.
+
+    Returns:
+        coverage_occurences_t2t(dictionary): a dictionary that contains the amount of times a amount of base coverage
+        occurs in the t2t bed file.
+        coverage_occurences_hg38(dictionary): a dictionary that contains the amount of times a amount of base coverage
+        occurs in the hg38 bed file.
+    """
+    # Turns the column "mean_coverage" into a list. This column contains the mean coverage of the bins.
     mean_coverage_t2t = dataframe_t2t["mean_coverage"].tolist()
     mean_coverage_hg38 = dataframe_hg38["mean_coverage"].tolist()
 
-
+    # Calculates amount of times an amount of mean coverage in the files.
     coverage_occurences_t2t = Counter(mean_coverage_t2t)
     coverage_occurences_hg38 = Counter(mean_coverage_hg38)
-    print(coverage_occurences_hg38)
-    print(coverage_occurences_t2t)
 
     return coverage_occurences_t2t, coverage_occurences_hg38
 
 
 def make_histogram(occurrences_coverage_t2t, occurrences_coverage_hg38):
+    """
+    Makes the histogram of the lists with the occurrences of the coverage amounts.
 
-    # fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=True)
+    Args:
+        occurrences_coverage_t2t(dictionary): a dictionary that contains the amount of times an amount of coverage was
+        performed on a base pair.
+        occurrences_coverage_hg38 (dictionary): a dictionary that contains the amount of times an amount of coverage was
+        performed on a base pair.
 
-
-    # titles = {ax1: "Mean coverage occurrence (CHM13)", ax2: "Mean coverage occurrences (GRCh38)"}
-    #
-    # ax = plt.gca()
-
-    # ax1.hist(coverage_occurences_t2t, color="blue")
-    # ax2.hist(coverage_occurences_hg38, color="red")
-    #
-    # ax1.set_ylabel("Number of variants in the CHM13")
-    # ax2.set_ylabel("Number of variants in the GRCh38")
-
-    plt.hist(occurrences_coverage_t2t, color="blue", bins="auto", label="T2T", alpha=0.5, histtype="step", )
+    :return:
+        saves the plot as coverage_occurrences_histogram.jpg.
+    """
+    # Makes a histogram with each file having a different color, automated length of values on the x-axis (bins),
+    # a label for the legend, a different alpha value and histtype to make the histograms easier to differentiate
+    plt.hist(occurrences_coverage_t2t, color="blue", bins="auto", label="T2T", alpha=0.5, histtype="step")
     plt.hist(occurrences_coverage_hg38,color="green", bins="auto", label="GRCh38", alpha=0.25, histtype="stepfilled")
+
+    # Makes a legend in the upper right
     plt.legend(loc="upper right")
+
+    # Writes the title for the figure in the figure
     plt.title("Mean coverage occurences")
+
+    # Writes the x and y labels in the figure
     plt.xlabel("Mean coverage")
     plt.ylabel("Number of occurrences")
 
+    # Saves the figure in a file
     plt.savefig("coverage_occurrences_histogram.jpg", bbox_inches="tight")
+
+    # Makes sure the plot is shown
     plt.show()
 
 
 if __name__ == "__main__":
     t2t_dataframe, hg38_dataframe = open_file()
-    coverage_occurences_t2t, coverage_occurences_hg38 = making_bins(t2t_dataframe, hg38_dataframe)
+    coverage_occurences_t2t, coverage_occurences_hg38 = Counting_the_coverage(t2t_dataframe, hg38_dataframe)
     make_histogram(coverage_occurences_t2t, coverage_occurences_hg38)
--- mismatch_plot/make_mismatch_plot.py
+++ mismatch_plot/make_mismatch_plot.py
@@ -1,16 +1,13 @@
 import os
-import bamnostic as bs  #bamnostic-1.1.10
-import numpy as np
+import bamnostic as bs
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
-# C:\Users\Z659216\Desktop\BAM_files\P3-D10.aligned.HiFi.bam
-# C:\Users\Z659216\Desktop\BAM_files\P3-D10.haplotagged.bam
 
 def read_files():
-    """This function asks for two paths: one to the t2t bam file and one to the hg38 file of the same sample and
+    """
+    This function asks for two paths: one to the t2t bam file and one to the hg38 file of the same sample and
     then adds the reads to list depending on which file the sample is from and then returns the lists.
 
     Args:
@@ -20,19 +17,22 @@ def read_files():
         hg38_reads(list): list of reads from the hg38 bam file.
         t2t_reads(list): list of reads from the t2t bam file.
     """
-
+    # lists where the reads will be appended into
     t2t_reads = []
     hg38_reads = []
+
+    # Asks for the input path for the t2t BAM file
     input_path_t2t_bam = input("Give the path to the t2t BAM file: ")
     print("given path: " + input_path_t2t_bam)
 
+    # Checks if there is a file in the given file path
     if os.path.exists(input_path_t2t_bam):
         print("File exists")
         print("Processing the reads")
         print(" ")
-        # Opens the file and turns it into a pandas dataframe.
+        # Opens the file and loops through it so it can add the reads into the bam_t2t list
         with bs.AlignmentFile(input_path_t2t_bam, "rb") as bam_t2t:
-            for read in bam_t2t.head(n=15000):
+            for read in bam_t2t:
                 t2t_reads.append(read)
 
         bam_t2t.close()
@@ -42,18 +42,22 @@ def read_files():
     else:
         print("The specified path does NOT exist!")
 
+    # Asks for the input path for the GRCh38 BAM file
     input_path_hg38_bam = input("Give the path to the GRCh38 BAM file: ")
     print("given path: " + input_path_hg38_bam)
 
+    # Checks if there is a file in the given path
     if os.path.exists(input_path_hg38_bam):
         print("File exists")
         print("Processing the reads")
         print(" ")
+        # Opens the file and loops through it so it can add the reads into the bam_hg38 list
         with bs.AlignmentFile(input_path_hg38_bam, "rb") as bam_hg38:
-            for read in bam_hg38.head(n=15000):
+            for read in bam_hg38:
                 hg38_reads.append(read)
         bam_hg38.close()
 
+    # prints the string if the file path doesn't exist
     else:
         print("The specified path does NOT exist!")
 
@@ -62,7 +66,8 @@ def read_files():
     return hg38_reads, t2t_reads
 
 def get_necessary_data(reads_hg38, reads_t2t):
-    """This function gets the lists with reads and searches per read for the NM tag (contains the number of mismatches)
+    """
+    This function gets the lists with reads and searches per read for the NM tag (contains the number of mismatches)
     and the length of the alignment part of the read. Then it calculates the mismatch rate by dividing the number of
     mismatches by the length of the alignment part of the read. Returns the lists with the mismatches rates for the
     two files.
@@ -72,24 +77,36 @@ def get_necessary_data(reads_hg38, reads_t2t):
         reads_t2t (list): list of reads from the t2t bam file.
 
     Returns:
-        mismatch_rate_list_hg38(list): list of mismatches rates from the hg38 bam file.
-        mismatch_rate_list_t2t(list): list of mismatches rates from the t2t bam file.
+        mean_mismatch_hg38(float): the average of the mismatch rate across the entire file. Calculated by dividing the
+        sum of mismatches by the length of the list with reads.
+        mean_mismatch_t2t(float): the average of the mismatch rate across the entire file. Calculated by dividing the
+        sum of mismatches by the length of the list with reads
     """
 
+    # The lists that will contain the mismatch rates
     mismatch_rate_list_t2t = []
     mismatch_rate_list_hg38 = []
 
+    # Loops through the list with the reads from the hg38 BAM file and gets the number of mismatches from the reads
+    # by getting the NM tag. Calculates the mismatch rate of the read by dividing the number of mismatches in the read
+    # by the length of the aligned portion of the read. Only if read has a tag
     for read in reads_hg38:
         mismatch_tag = read.get_tag("NM")
         if mismatch_tag is not None:
             mismatch_rate = mismatch_tag / read.query_alignment_length
             mismatch_rate_list_hg38.append(mismatch_rate)
 
+    # Loops through the list with the reads from the t2t BAM file and gets the number of mismatches from the reads
+    # by getting the NM tag. Calculates the mismatch rate of the read by dividing the number of mismatches in the read
+    # by the length of the aligned portion of the read. Only if read has a tag
     for read in reads_t2t:
         mismatch_tag = read.get_tag("NM")
         if mismatch_tag is not None:
             mismatch_rate = mismatch_tag / read.query_alignment_length
             mismatch_rate_list_t2t.append(mismatch_rate)
+
+    # Calculates the average mismatch rate of the files by dividing the sum of mismatches in their respective list by
+    # the length of the list with the reads from that file.
     mean_mismatch_hg38 = sum(mismatch_rate_list_hg38) / len(reads_hg38)
     mean_mismatch_t2t = sum(mismatch_rate_list_t2t) / len(reads_t2t)
 
@@ -97,22 +114,40 @@ def get_necessary_data(reads_hg38, reads_t2t):
     return mean_mismatch_hg38, mean_mismatch_t2t
 
 def plot_boxplot_comparison(mismatch_mean_hg38, mismatch_mean_t2t):
-    """Uses the lists with mismatch rate to plot a violin plot. Use is for comparison between the two reference genomes.
+    """
+    Uses the lists with mismatch rate to plot a violin plot. Use is for comparison between the two reference genomes.
 
     Args:
-        mismatch_rate_t2t_list(list): list of mismatches rates from the t2t bam file.
-        mismatch_rate_hg38_list(list): list of mismatches rates from the hg38 bam file.
+        mismatch_mean_hg38(float): the average of the mismatch rate across the entire file. Calculated by dividing the
+        sum of mismatches by the length of the list with reads.
+        mismatch_mean_t2t(float): the average of the mismatch rate across the entire file. Calculated by dividing the
+        sum of mismatches by the length of the list with reads.
+
+    Returns:
+        saves the plot as mismatch_rate_comparison.jpg.
     """
+    # Makes a dataframe containing the average mismatches of the files.
     dataframe_combined_mismatches = pd.DataFrame({"T2T": [mismatch_mean_t2t], "Hg38": [mismatch_mean_hg38]},
                                                  columns=["T2T", "Hg38"])
+
+    # Sets the figure up with the width and heigth
     fig, ax =plt.subplots(figsize=(5, 4))
 
+    # Creates a scatter plot with the name of the reference genomes in the x-axis and the average mismatch rates in
+    # the y-axis in the figure
     ax.scatter(x=dataframe_combined_mismatches.keys(), y=dataframe_combined_mismatches.values)
+
+    # Sets a limit to the x-axis and y-axis
     ax.set_xlim(-0.50, 1.50)
     ax.set_ylim(0, 0.04)
+
+    # Sets the x labels up
     ax.set_xticklabels(["T2T", "Hg38"])
 
+    # Sets the title up
     plt.title("Average mismatch rate")
+
+    # Saves the figure in a jpg file
     plt.savefig("mismatch_rate_comparison.jpg")
 
     plt.show()
