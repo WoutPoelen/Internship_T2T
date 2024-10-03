263ff5d8a37cb3c891c857755ef535fac624189c
WoutPoelen <Wout.Poelen@radboudumc.nl>
2024-10-03 11:18:49 +0200

    Added print statements to show progress during the running

--- low_coverage_comparison/comparing_low_coverage_regions.py
+++ low_coverage_comparison/comparing_low_coverage_regions.py
@@ -6,14 +6,18 @@ import pandas as pd
 
 def generate_dataframes(args):
 
+
     t2t_dataframe = pd.read_csv(args.T2T_BED_file, sep="\t", encoding="utf-8")
     hg38_dataframe = pd.read_csv(args.GRCh38_BED_file, sep="\t", encoding="utf-8")
     liftover_dataframe = pd.read_csv(args.Liftover_BED_file, sep="\t", encoding="utf-8")
+
+    print("Reading the files")
 
     t2t_dataframe.columns = ["Chromosome", "Start", "End", "mean_coverage"]
     hg38_dataframe.columns = ["Chromosome", "Start", "End", "mean_coverage"]
     liftover_dataframe.columns = ["Chromosome", "Start", "End", "mean_coverage", "succesful_liftover"]
 
+    print("Processing the files")
     hg38_dataframe = hg38_dataframe[(hg38_dataframe["End"] <= 5000000) & (hg38_dataframe["Chromosome"] == "chr1") &
                                     (hg38_dataframe["mean_coverage"] <= 10)]
     t2t_dataframe = t2t_dataframe[(t2t_dataframe["End"] <= 5000000) & (t2t_dataframe["Chromosome"] == "chr1") &
@@ -25,6 +29,9 @@ def generate_dataframes(args):
 def plot_low_coverage(dataframe_t2t, dataframe_hg38, dataframe_liftover):
     # Makes the plotting work on the server
     matplotlib.use("Agg")
+
+    print("Generating the plot")
+
     dataframe_t2t["Reference genome"] = "T2T"
     dataframe_hg38["Reference genome"] = "GRCh38"
     dataframe_liftover["Reference genome"] = "liftover_T2T"
@@ -38,8 +45,9 @@ def plot_low_coverage(dataframe_t2t, dataframe_hg38, dataframe_liftover):
 
     plt.xlabel("Position (Mbp)")
 
-    plt.savefig("low_coverage_chr1_subset.png")
+    plt.savefig("low_coverage_comparison.png")
 
+    print("The low coverage plot has been successfully generated and save as low_coverage_comparison.png")
     # plt.show()
 
 
