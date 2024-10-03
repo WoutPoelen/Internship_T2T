8684df9f0147e5ede73e2b02a63f53dbbe3bb22c
WoutPoelen <Wout.Poelen@radboudumc.nl>
2024-10-03 11:42:17 +0200

    Added argparse to make it work better with the server.
    Added print statements to make the process more clear during the running
    
    Change the plot into a bar plot

--- mismatch_plot/make_mismatch_plot.py
+++ mismatch_plot/make_mismatch_plot.py
@@ -1,10 +1,11 @@
+import argparse
 import os
 import bamnostic as bs
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 
-def read_files():
+def read_files(arguments):
     """
     This function asks for two paths: one to the t2t bam file and one to the hg38 file of the same sample and
     then adds the reads to list depending on which file the sample is from and then returns the lists.
@@ -20,45 +21,21 @@ def read_files():
     t2t_reads = []
     hg38_reads = []
 
-    # Asks for the input path for the t2t BAM file
-    input_path_t2t_bam = input("Give the path to the t2t BAM file: ")
-    print("given path: " + input_path_t2t_bam)
-
-    # Checks if there is a file in the given file path
-    if os.path.exists(input_path_t2t_bam):
-        print("File exists")
-        print("Processing the reads")
-        print(" ")
-        # Opens the file and loops through it so it can add the reads into the bam_t2t list
-        with bs.AlignmentFile(input_path_t2t_bam, "rb") as bam_t2t:
-            for read in bam_t2t:
-                t2t_reads.append(read)
+    print("Reading the files")
+    #     # Opens the file and loops through it so it can add the reads into the bam_t2t list
+    with bs.AlignmentFile(arguments.T2T_BAM_file, "rb") as bam_t2t:
+        for read in bam_t2t.head(10):
+            t2t_reads.append(read)
 
         bam_t2t.close()
 
-
-    # Prints the string if the file path doesn't exist
-    else:
-        print("The specified path does NOT exist!")
-
-    # Asks for the input path for the GRCh38 BAM file
-    input_path_hg38_bam = input("Give the path to the GRCh38 BAM file: ")
-    print("given path: " + input_path_hg38_bam)
-
-    # Checks if there is a file in the given path
-    if os.path.exists(input_path_hg38_bam):
-        print("File exists")
-        print("Processing the reads")
-        print(" ")
-        # Opens the file and loops through it so it can add the reads into the bam_hg38 list
-        with bs.AlignmentFile(input_path_hg38_bam, "rb") as bam_hg38:
-            for read in bam_hg38:
+    #     # Opens the file and loops through it so it can add the reads into the bam_hg38 list
+        with bs.AlignmentFile(arguments.GRCh38_BAM_file, "rb") as bam_hg38:
+            for read in bam_hg38.head(10):
                 hg38_reads.append(read)
+
         bam_hg38.close()
 
-    # prints the string if the file path doesn't exist
-    else:
-        print("The specified path does NOT exist!")
 
     return hg38_reads, t2t_reads
 
@@ -83,6 +60,8 @@ def get_necessary_data(reads_hg38, reads_t2t):
     # The lists that will contain the mismatch rates
     mismatch_rate_list_t2t = []
     mismatch_rate_list_hg38 = []
+
+    print("Calculating the mismatch rates")
 
     # Loops through the list with the reads from the hg38 BAM file and gets the number of mismatches from the reads
     # by getting the NM tag. Calculates the mismatch rate of the read by dividing the number of mismatches in the read
@@ -122,21 +101,28 @@ def plot_boxplot_comparison(mismatch_mean_hg38, mismatch_mean_t2t):
     Returns:
         saves the plot as mismatch_rate_comparison.jpg.
     """
+
     matplotlib.use("Agg")
 
-    # Makes a dataframe containing the average mismatches of the files.
-    dataframe_combined_mismatches = pd.DataFrame({"T2T": [mismatch_mean_t2t], "Hg38": [mismatch_mean_hg38]},
-                                                 columns=["T2T", "Hg38"])
+    print("Generating the plot")
 
-    # Sets the figure up with the width and heigth
+    colors = ["blue", "red"]
+    # Makes a dataframe containing the average mismatches of the files.
+    dataframe_combined_mismatches = pd.DataFrame = pd.DataFrame({"T2T": [mismatch_mean_t2t],
+                                                                 "Hg38": [mismatch_mean_hg38]
+                                                                 })
+
+
+    # Sets the figure up with the width and height
     fig, ax =plt.subplots(figsize=(5, 4))
 
     # Creates a scatter plot with the name of the reference genomes in the x-axis and the average mismatch rates in
     # the y-axis in the figure
-    ax.scatter(x=dataframe_combined_mismatches.keys(), y=dataframe_combined_mismatches.values)
+    ax.bar(x=dataframe_combined_mismatches.columns, height=dataframe_combined_mismatches.iloc[0], color=colors,
+           width=0.5)
 
     # Sets a limit to the x-axis and y-axis
-    ax.set_xlim(-0.50, 1.50)
+    ax.set_xlim(-0.5, 1.5)
     ax.set_ylim(0, 0.04)
 
     # Sets the x labels up
@@ -146,14 +132,27 @@ def plot_boxplot_comparison(mismatch_mean_hg38, mismatch_mean_t2t):
     plt.title("Average mismatch rate")
 
     # Saves the figure in a jpg file
-    plt.savefig("mismatch_rate_comparison.jpg")
+    plt.savefig("mismatch_rate_comparison.png")
+
+    print("The barplot has been generated and is saved as mismatch_rate_comparison.png")
 
     # Makes sure the plot is shown. Commented out because file wouldn't be saved on the server. Get rid of the comment
     # when you want to see the file outside the server
     # plt.show()
 
-
-if __name__ == "__main__":
-    hg38_reads, t2t_reads = read_files()
+def main(args):
+    hg38_reads, t2t_reads = read_files(args)
     mean_mismatch_hg38, mean_mismatch_t2t = get_necessary_data(hg38_reads, t2t_reads)
     plot_boxplot_comparison(mean_mismatch_hg38, mean_mismatch_t2t)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("T2T_BAM_file",
+                        help="Path to the BAM file of the sample that is mapped to T2T",
+                        metavar="the T2T input BAM file")
+    parser.add_argument("GRCh38_BAM_file",
+                        help="Path to the BAM file of the sample that is mapped to T2T",
+                        metavar="the GRCh38 input BAM file")
+    args = parser.parse_args()
+
+    main(args)
