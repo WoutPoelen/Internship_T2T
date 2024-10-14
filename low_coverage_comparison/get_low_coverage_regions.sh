fed7ef26cc53297fc7b4c6549c4119bc7bdf14d8
WoutPoelen <Wout.Poelen@radboudumc.nl>
2024-10-14 15:23:28 +0200

    Updated the output echo

--- low_coverage_comparison/get_low_coverage_regions.sh
+++ low_coverage_comparison/get_low_coverage_regions.sh
@@ -13,5 +13,5 @@ echo $T2T_GENOME_REGIONS_INPUT_FILE "has been processed and the output has been 
 
 # Processes the from Mosdepth obtained GRCh38 gz file and gets the regions which have a average coverage lower than 10
 zcat $GRCh38_GENOME_REGIONS_INPUT | awk '{FS="\t";OFS="\t"} ($4 <10) {print $1,$2,$3,$4}' > $GRCh38_LOW_COVERAGE_REGION_OUTPUT
-echo GRCh38_GENOME_REGIONS_INPUT "has been processed and the output has been send to" $GRCh38_LOW_COVERAGE_REGION_OUTPUT
+echo $GRCh38_GENOME_REGIONS_INPUT "has been processed and the output has been send to" $GRCh38_LOW_COVERAGE_REGION_OUTPUT
 
