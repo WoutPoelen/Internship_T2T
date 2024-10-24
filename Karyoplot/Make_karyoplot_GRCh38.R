bc9ba94d6b49eb0d20f06029f2a1a29279ad4836
WoutPoelen <90319381+WoutPoelen@users.noreply.github.com>
2024-10-24 16:13:35 +0200

    Update Make_karyoplot_GRCh38.R
    Changed it to the right file.

--- Karyoplot/Make_karyoplot_GRCh38.R
+++ Karyoplot/Make_karyoplot_GRCh38.R
@@ -1,18 +1,10 @@
 library(karyoploteR)
 library(GenomicRanges)
 
-data_T2T <- toGRanges(data.frame(chr=fix_T2T_entire_genome_low_coverage["V1"], 
-                             start=fix_T2T_entire_genome_low_coverage["V2"], 
-                             end=fix_T2T_entire_genome_low_coverage["V3"],
-                             y=fix_T2T_entire_genome_low_coverage["V4"]))
-
-custom_genome <- toGRanges(data.frame(chr=chromosome_report_t2t$UCSC.style.name,
-                                    start=0, 
-                                    end=chromosome_report_t2t$Seq.length))
-
-custom_cytobands <- toGRanges(data.frame(chr=filtered_centromeres_CenSat$V1,
-                                         start=filtered_centromeres_CenSat$V2,
-                                         end=filtered_centromeres_CenSat$V3))
+data_GRCh38 <- toGRanges(data.frame(chr=GRCh38_entire_genome_P3.D10_low_coverage["V1"], 
+                                  start=GRCh38_entire_genome_P3.D10_low_coverage["V2"], 
+                                  end=GRCh38_entire_genome_P3.D10_low_coverage["V3"],
+                                  y=GRCh38_entire_genome_P3.D10_low_coverage["V4"]))
 
 plot_params <- getDefaultPlotParams(plot.type = 1)
 
@@ -22,12 +14,11 @@ plot_params$leftmargin <- 0.06
 plot_params$ideogramlateralmargin <- 0.05
 plot_params$data.panel.margin <- 0.1
 
-kp_t2t <- plotKaryotype(genome = custom_genome,plot.type = 1, 
-                        chromosomes = "all", plot.params = plot_params)
-kpAddBaseNumbers(kp_t2t)
-kpPlotRegions(kp_t2t, data = custom_cytobands, data.panel = "ideogram", 
-              col = "red")
-kpPlotDensity(kp_t2t, data=data_T2T, data.panel = 1, window.size = 5000, 
+kp_GRCh38 <- plotKaryotype(genome = "hg38", plot.params = plot_params, 
+                           plot.type = 1)
+kpAddBaseNumbers(kp_GRCh38)
+kpPlotDensity(kp_GRCh38, data=data_GRCh38, data.panel = 1, window.size = 5000,
               col="black")
-kpAddMainTitle(kp_t2t, "Low coverage regions T2T-CHM13", col="black")
+kpAddMainTitle(kp_GRCh38, "Low coverage regions GRCh38", col="black")
+
 
