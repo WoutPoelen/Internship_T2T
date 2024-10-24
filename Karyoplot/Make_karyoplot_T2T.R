7e41cf34aceceea3793af18ed0cb7f3b614acfc5
WoutPoelen <90319381+WoutPoelen@users.noreply.github.com>
2024-10-24 16:14:10 +0200

    Update Make_karyoplot_T2T.R
    Changed it to the file that cannot be run on the server.

--- Karyoplot/Make_karyoplot_T2T.R
+++ Karyoplot/Make_karyoplot_T2T.R
@@ -1,36 +1,18 @@
 library(karyoploteR)
 library(GenomicRanges)
 
-args <- commandArgs(trailingOnly = TRUE)
+data_T2T <- toGRanges(data.frame(chr=fix_T2T_entire_genome_low_coverage["V1"], 
+                             start=fix_T2T_entire_genome_low_coverage["V2"], 
+                             end=fix_T2T_entire_genome_low_coverage["V3"],
+                             y=fix_T2T_entire_genome_low_coverage["V4"]))
 
-file_path_t2t_low_coverage <- args[1]
-chromosome_locations <- args[2]
-centromeres_locations <- args[3]
-path_to_plot <- args[4]
+custom_genome <- toGRanges(data.frame(chr=chromosome_report_t2t$UCSC.style.name,
+                                    start=0, 
+                                    end=chromosome_report_t2t$Seq.length))
 
-png(path_to_plot)
-
-bed_data <- read.table(file_path_t2t_low_coverage, header = FALSE, sep = "\t")
-
-chromosomes <- read.table(chromosome_locations, header = TRUE, sep = "\t")
-
-centromeres <- read.table(centromeres_locations, header = FALSE, sep = "\t")
-
-colnames(bed_data) <- c("chromosome", "start", "end", "mean_coverage")
-colnames(centromeres) <- c("chromosome", "start", "end") 
-
-data_T2T <- toGRanges(data.frame(chr = bed_data$chromosome, 
-                                 start = bed_data$start, 
-                                 end = bed_data$end,
-                                 y = bed_data$mean_coverage))
-
-custom_genome <- toGRanges(data.frame(chr = chromosome_report_t2t$UCSC.style.name,
-                                      start = 0, 
-                                      end = chromosome_report_t2t$Seq.length))
-
-custom_cytobands <- toGRanges(data.frame(chr = centromeres$start,
-                                         start = centromeres$start,
-                                         end = centromeres$end))
+custom_cytobands <- toGRanges(data.frame(chr=filtered_centromeres_CenSat$V1,
+                                         start=filtered_centromeres_CenSat$V2,
+                                         end=filtered_centromeres_CenSat$V3))
 
 plot_params <- getDefaultPlotParams(plot.type = 1)
 
@@ -49,4 +31,3 @@ kpPlotDensity(kp_t2t, data=data_T2T, data.panel = 1, window.size = 5000,
               col="black")
 kpAddMainTitle(kp_t2t, "Low coverage regions T2T-CHM13", col="black")
 
-dev.off()
