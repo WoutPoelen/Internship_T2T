9166047d7ab2885283cc37775b0288b3c5207e14
WoutPoelen <90319381+WoutPoelen@users.noreply.github.com>
2024-10-29 09:38:53 +0100

    Update karyoplot_GRCh38.R

--- Karyoplot/karyoplot_GRCh38.R
+++ Karyoplot/karyoplot_GRCh38.R
@@ -1,10 +1,10 @@
 library(karyoploteR)
 library(GenomicRanges)
 
-data_GRCh38 <- toGRanges(data.frame(chr=GRCh38_entire_genome_P3.D10_low_coverage["V1"], 
-                                  start=GRCh38_entire_genome_P3.D10_low_coverage["V2"], 
-                                  end=GRCh38_entire_genome_P3.D10_low_coverage["V3"],
-                                  y=GRCh38_entire_genome_P3.D10_low_coverage["V4"]))
+data_GRCh38 <- toGRanges(data.frame(chr=GRCh38_low_coverage_HG002["V1"], 
+                                  start=GRCh38_low_coverage_HG002["V2"], 
+                                  end=GRCh38_low_coverage_HG002["V3"],
+                                  y=GRCh38_low_coverage_HG002["V4"]))
 
 plot_params <- getDefaultPlotParams(plot.type = 1)
 
