c0dd89ed4c9d78974ba7a32fdcbb56428a97a2c9
WoutPoelen <Wout.Poelen@radboudumc.nl>
2024-11-18 16:27:17 +0100

    Fixed a bug where in step 3 it would get genes that would only partially match but would not be in the other file

--- low_coverage_comparison/categories_overlap_with_low_coverage/Genes_in_low_coverage_regions/low_coverage_CDS_regions.sh
+++ low_coverage_comparison/categories_overlap_with_low_coverage/Genes_in_low_coverage_regions/low_coverage_CDS_regions.sh
@@ -33,8 +33,8 @@ echo "The coding sequence rows have been obtained and sent to: $T2T_CDS_genes , 
 
 # Step 3: Getting the rows with the Gene_id's which are in the other RefSeq file.
 # This is done to stop the difference from triggering due to a name difference.
-awk '{print $4}' "$GRCh38_CDS_genes" | uniq | grep -Ff -  "$T2T_CDS_genes" | sort -k1,1V -k2,2n > "$T2T_CDS_GeneID_Overlapping"
-awk '{print $4}' "$T2T_CDS_genes"  | uniq | grep -Ff -  "$GRCh38_CDS_genes" | sort -k1,1V -k2,2n > "$GRCh38_CDS_GeneID_Overlapping"
+awk '{print $4}' "$GRCh38_CDS_genes" | uniq | grep -Fw -f -  "$T2T_CDS_genes" | sort -k1,1V -k2,2n > "$T2T_CDS_GeneID_Overlapping"
+awk '{print $4}' "$T2T_CDS_genes"  | uniq | grep -Fw -f -  "$GRCh38_CDS_genes" | sort -k1,1V -k2,2n > "$GRCh38_CDS_GeneID_Overlapping"
 echo "The rows with the Gene_ids which are in both files are gotten and sent to: $T2T_CDS_GeneID_Overlapping , $GRCh38_CDS_GeneID_Overlapping"
 
 # Step 4: Intersecting the regions from the RefSeq files and their Gene_id's with the low coverage regions found in the
